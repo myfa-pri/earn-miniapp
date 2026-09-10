09a4069a85c336fed467a678bb1261e4fcd26c3f
MYFA PRIMINIUM <yichu.myfa@gmail.com>
2026-09-10 12:39:49 +0300

    Serve final MYFA BB frontend JavaScript

--- bots-business/commands/renderJS.js
+++ bots-business/commands/renderJS.js
@@ -1,6 +1,6 @@
 /*CMD
   command: renderJS
-  help: MYFA BIRR JS renderer
+  help: MYFA BIRR final JS renderer
   need_reply:
   auto_retry_time:
   folder: MYFA WEBAPP
@@ -9,7 +9,4 @@
   aliases:
   group:
 CMD*/
-WebApp.render({
-  template: "script.bb.js",
-  mime_type: "application/javascript"
-});
+WebApp.render({template:"script.final.js",mime_type:"application/javascript"});
